c6f9f7b4ec4b82df3684bd45c3b38f17b4cddd53
Justin Thomas <thomasjustin2203@gmail.com>
2024-01-24 12:52:50 +0530

    Initial Commit of docs application

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_docs/screens/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Google Docs',
+      home: LoginScreen(),
+    );
+  }
+}
